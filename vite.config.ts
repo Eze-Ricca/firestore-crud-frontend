aa0d9ca0a4ba3d087d0a1eeee769c5a6a2891a04
Eze-Ricca <riccaesteban@gmail.com>
2025-05-14 12:34:48 -0300

    second commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/nombre-repo/", // Reemplaza 'nombre-repo' por el nombre de tu repositorio
+  base: "/firestore-crud-frontend/", // Reemplaza 'nombre-repo' por el nombre de tu repositorio
   build: {
     outDir: "dist",
   },
